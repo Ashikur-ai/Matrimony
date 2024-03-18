3fc0e5afd29ade90d557dafb52eb71b4386a5d7f
ashiku2 <ashikur1602@gmail.com>
2024-03-18 12:33:54 +0600

    Footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Footer';
 
 const Main = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
