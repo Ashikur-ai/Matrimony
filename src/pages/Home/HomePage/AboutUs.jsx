e02f82856bfef02bbc2de78030192fc7166cbe8e
ashiku2 <ashikur1602@gmail.com>
2024-03-19 00:01:04 +0600

    About us page added

--- src/pages/Home/HomePage/AboutUs.jsx
+++ src/pages/Home/HomePage/AboutUs.jsx
@@ -1,10 +1,133 @@
 import React from 'react';
+import Header from '../../../Components/Header';
+import { Helmet } from 'react-helmet-async';
 
 const AboutUs = () => {
     return (
-        <div>
-            About us
-        </div>
+        <>
+            <Helmet>
+                <title>About Us | MatriMony</title>
+            </Helmet>
+            <Header></Header>
+            <section className="text-gray-600 body-font min-h-screen">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-20">
+                        <h1 className="text-2xl font-medium title-font mb-4 text-gray-900 tracking-widest">OUR TEAM</h1>
+                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Meet the dedicated individuals behind the scenes who make our mission possible. Our team is committed to providing you with exceptional service and support throughout your journey. With expertise in matchmaking and a passion for fostering meaningful connections, we're here to guide you every step of the way.</p>
+                    </div>
+                    <div className="flex flex-wrap -m-4">
+                        <div className="p-4 lg:w-1/2">
+                            <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                                <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://i.ibb.co/thbF5qK/3.jpg"/>
+                                    <div className="flex-grow sm:pl-8">
+                                        <h2 className="title-font font-medium text-lg text-gray-900">Holden Caulfield</h2>
+                                    <h3 className="text-gray-500 mb-3">Matchmaking Specialists</h3>
+                                    <p className="mb-4">Meet our team of seasoned Matchmaking Specialists, experts dedicated to crafting personalized connections tailored to your unique preferences and desires. With a keen eye for compatibility and a passion for fostering lasting relationships, they're committed to guiding you on your journey to finding love. Trust in their expertise and let them help you navigate the path to your perfect match.</p>
+                                        <span className="inline-flex">
+                                            <a className="text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                                </svg>
+                                            </a>
+                                            <a className="ml-2 text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                                </svg>
+                                            </a>
+                                            <a className="ml-2 text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                                </svg>
+                                            </a>
+                                        </span>
+                                    </div>
+                            </div>
+                        </div>
+                        <div className="p-4 lg:w-1/2">
+                            <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                                <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://i.ibb.co/4pKTPLh/1.jpg"/>
+                                    <div className="flex-grow sm:pl-8">
+                                        <h2 className="title-font font-medium text-lg text-gray-900">Alper Kamu</h2>
+                                    <h3 className="text-gray-500 mb-3">Relationship Coaches</h3>
+                                    <p className="mb-4">Empowering your journey towards love, our Relationship Coaches offer personalized guidance and support. With expertise in fostering healthy connections, they're here to help you navigate the complexities of relationships and discover lasting happiness</p>
+                                        <span className="inline-flex">
+                                            <a className="text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                                </svg>
+                                            </a>
+                                            <a className="ml-2 text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                                </svg>
+                                            </a>
+                                            <a className="ml-2 text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                                </svg>
+                                            </a>
+                                        </span>
+                                    </div>
+                            </div>
+                        </div>
+                        <div className="p-4 lg:w-1/2">
+                            <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                                <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://i.ibb.co/w662RPV/MT-FOS-TM23-04-F-192-18.webp"/>
+                                    <div className="flex-grow sm:pl-8">
+                                    <h2 className="title-font font-medium text-lg text-gray-900">Kisinger Paul</h2>
+                                    <h3 className="text-gray-500 mb-3">Technical Support Team</h3>
+                                    <p className="mb-4">Your satisfaction is our priority. Our dedicated Customer Support Representatives are here to assist you every step of the way. Reach out to us for prompt and personalized assistance with any questions or concerns.</p>
+                                        <span className="inline-flex">
+                                            <a className="text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                                </svg>
+                                            </a>
+                                            <a className="ml-2 text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                                </svg>
+                                            </a>
+                                            <a className="ml-2 text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                                </svg>
+                                            </a>
+                                        </span>
+                                    </div>
+                            </div>
+                        </div>
+                        <div className="p-4 lg:w-1/2">
+                            <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                                <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://i.ibb.co/HBkVhpC/PGDs1-Mioa-B3w-HNMCmkz-To-M0-CNtb21ud3-Kz-Yrqzp-N.jpg"/>
+                                    <div className="flex-grow sm:pl-8">
+                                        <h2 className="title-font font-medium text-lg text-gray-900">Henry Letham</h2>
+                                    <h3 className="text-gray-500 mb-3">Technical Support Team</h3>
+                                    <p className="mb-4">Tech hiccups? Don't fret. Our Technical Support Team is here to keep your experience seamless. Reach out anytime for swift resolution and smooth sailing on our platform</p>
+                                        <span className="inline-flex">
+                                            <a className="text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                                </svg>
+                                            </a>
+                                            <a className="ml-2 text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                                </svg>
+                                            </a>
+                                            <a className="ml-2 text-gray-500">
+                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                                                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                                </svg>
+                                            </a>
+                                        </span>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 };
 
